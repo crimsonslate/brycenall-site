eb9f4dd8af6c1ce749dbaaec750f4d354dfd459d
Blake Nall (darthnall) <dev@darthnall.com>
2024-10-08 17:37:58 -0500

    Added media models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.core.files.storage import storages
+from datetime import date
 
-# Create your models here.
+
+class PublishedMedia(models.Model):
+    # The media itself
+    source = models.FileField(storage=storages["bucket"])
+
+    # Info
+    title = models.CharField(max_length=256)
+    desc = models.TextField(verbose_name="description", max_length=2048)
+    datetime_published = models.DateTimeField(default=timezone.now())
+    datetime_last_modified = models.DateTimeField(auto_now=True)
+    date_created = models.DateField(default=date.today)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class PublishedVideo(PublishedMedia, models.Model):
+    ...
+
+
+class PublishedImage(PublishedMedia, models.Model):
+    ...
