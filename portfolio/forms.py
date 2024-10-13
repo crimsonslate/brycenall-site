e9317a5157d811cca9e6a2c4bc6a8a98cbe6f9db
Blake Nall (darthnall) <dev@darthnall.com>
2024-10-13 17:38:01 -0500

    Added newsletter signup form

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from django.forms.renderers import TemplatesSetting
 
@@ -8,6 +9,21 @@ class PortfolioFormRenderer(TemplatesSetting):
     form_template_name = "portfolio/forms/_form.html"
     formset_template_name = "portfolio/forms/_formset.html"
     field_template_name = "portfolio/forms/_field.html"
+
+
+class NewsletterSignupForm(forms.Form):
+    email = forms.EmailField(
+        label="Email Address",
+        error_messages={"required": "Please enter your email address."},
+    )
+    opted_in = forms.BooleanField(
+        label="Opted-In",
+        help_text="By checking this box, you agree to recieve marketing emails from us in the future.",
+        initial=False,
+        error_messages={
+            "required": "You must agree to recieve marking emails from us to sign up for our newsletter."
+        },
+    )
 
 
 class MediaUploadForm(ModelForm):
