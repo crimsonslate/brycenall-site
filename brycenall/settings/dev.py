052e3bfcea6ca75453f32e5154f733287bbf398c
Blake Nall (darthnall) <dev@darthnall.com>
2024-10-16 18:02:17 -0500

    Reenabled form renderer for real

--- brycenall/settings/dev.py
+++ brycenall/settings/dev.py
@@ -17,7 +17,7 @@ TIME_ZONE = "America/Chicago"
 USE_I18N = True
 USE_TZ = True
 WSGI_APPLICATION = "brycenall.wsgi.application"
-# FORM_RENDERER = "portfolio.forms.PortfolioFormRenderer"
+FORM_RENDERER = "portfolio.forms.PortfolioFormRenderer"
 
 DATABASES = {
     "default": {
