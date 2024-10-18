b2667694f5caffa67bd8bd7b66a5a9081b265622
Blake Nall (darthnall) <dev@darthnall.com>
2024-10-17 22:04:32 -0500

    Added PORTFOLIO_NAME setting

--- brycenall/settings/dev.py
+++ brycenall/settings/dev.py
@@ -18,6 +18,7 @@ USE_I18N = True
 USE_TZ = True
 WSGI_APPLICATION = "brycenall.wsgi.application"
 FORM_RENDERER = "portfolio.forms.PortfolioFormRenderer"
+PORTFOLIO_NAME = "Bryce Nall"
 
 DATABASES = {
     "default": {
