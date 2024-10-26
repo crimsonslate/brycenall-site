7cb37910cd12ce77a1d30c03fd92aae87c1606c9
Blake Nall (darthnall) <dev@darthnall.com>
2024-10-26 14:09:18 -0500

    Fixed update media form

--- portfolio/forms/forms.py
+++ portfolio/forms/forms.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django import forms
 from django.core.validators import validate_image_file_extension
 from django.utils.translation import gettext_lazy as _
@@ -39,3 +41,29 @@ class MediaEditForm(forms.Form):
 
 class MediaUploadForm(forms.Form):
     default_renderer = PortfolioFormRenderer
+
+    source = forms.FileField(
+        label=_("Source"),
+        help_text=_("Upload a video or an image."),
+        widget=FileInput(),
+        allow_empty_file=False,
+        validators=[validate_media_file_extension],
+    )
+    thumb = forms.FileField(
+        label=_("Thumbnail"),
+        help_text=_("Upload an optional thumbnail."),
+        widget=FileInput(),
+        allow_empty_file=False,
+        validators=[validate_image_file_extension],
+    )
+    title = forms.CharField(label=_("Title"), help_text=_(""), widget=TextInput())
+    subtitle = forms.CharField(label=_("Subtitle"), help_text=_(""), widget=TextInput())
+    desc = forms.CharField(
+        label=_("Description"), help_text=_(""), widget=TextareaInput()
+    )
+    is_hidden = forms.FileField(
+        label=_("Set as hidden?"), help_text=_(""), widget=CheckboxInput()
+    )
+    is_image = forms.FileField(
+        label=_("Set as image?"), help_text=_(""), widget=CheckboxInput()
+    )
