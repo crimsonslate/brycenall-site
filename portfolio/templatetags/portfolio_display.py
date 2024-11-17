b0ed434ae5e52305de9d7e3abe8a8f17896fceef
Blake Nall (darthnall) <dev@darthnall.com>
2024-11-16 19:32:06 -0600

    Updated gallery display template tag

--- portfolio/templatetags/portfolio_display.py
+++ portfolio/templatetags/portfolio_display.py
@@ -5,16 +5,16 @@ from portfolio.models import Media
 register = Library()
 
 
-@register.inclusion_tag("portfolio/media/display.html")
-def display(media: Media, css_class: str | None = None) -> dict[str, str | bool | None]:
+@register.inclusion_tag("portfolio/media/gallery_display.html")
+def gallery_display(
+    media: Media, css_class: str | None = None
+) -> dict[str, str | bool | None]:
     return {
-        "url": media.url,
+        "url": media.source.url if media.is_image else media.thumb.url,
         "detail_url": media.get_absolute_url(),
         "alttext": media.title,
         "image": media.is_image,
-        "class": css_class if css_class else "",
-        "height": str(1024),
-        "width": str(1024),
+        "class": css_class,
     }
 
 
