bfb677df3a626182774821eef846fb200a4b4ad6
Blake Nall (darthnall) <dev@darthnall.com>
2024-11-18 16:52:15 -0600

    Added search result tag

--- portfolio/templatetags/portfolio_display.py
+++ portfolio/templatetags/portfolio_display.py
@@ -15,6 +15,16 @@ def display_gallery(media: Media, css_class: str | None = None) -> dict[str, Any
     }
 
 
+@register.inclusion_tag("portfolio/media/display_search_result.html")
+def display_search_result(media: Media, css_class: str | None = None) -> dict[str, Any]:
+    return {
+        "source_url": media.source.url if media.is_image else media.thumb.url,
+        "detail_url": media.get_absolute_url(),
+        "title": media.title,
+        "class": css_class,
+    }
+
+
 @register.inclusion_tag("portfolio/media/display_detail.html")
 def display_detail(media: Media, css_class: str | None = None) -> dict[str, Any]:
     width, height = media.dimensions
