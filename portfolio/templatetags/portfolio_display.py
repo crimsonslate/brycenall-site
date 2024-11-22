cb8b4ef7d03d5472fcda760eb3717cfeea345c9c
Blake Nall (darthnall) <dev@darthnall.com>
2024-11-21 18:48:09 -0600

    Updated media display tag

--- portfolio/templatetags/portfolio_display.py
+++ portfolio/templatetags/portfolio_display.py
@@ -6,36 +6,18 @@ from portfolio.models import Media
 register = Library()
 
 
-@register.inclusion_tag("portfolio/media/display_gallery.html")
-def display_gallery(media: Media, css_class: str | None = None) -> dict[str, Any]:
-    return {
-        "url": media.source.url if media.is_image else media.thumb.url,
-        "alttext": media.title,
-        "class": css_class,
-    }
+@register.inclusion_tag("portfolio/media/display.html")
+def display(
+    media: Media, force_image: bool = False, css_class: str | None = None
+) -> dict[str, Any]:
+    source_url: str = media.source.url
+    if not media.is_image and force_image:
+        source_url: str = media.thumb.url
 
-
-@register.inclusion_tag("portfolio/media/display_search_result.html")
-def display_search_result(media: Media, css_class: str | None = None) -> dict[str, Any]:
     return {
-        "source_url": media.source.url if media.is_image else media.thumb.url,
-        "detail_url": media.get_absolute_url(),
         "title": media.title,
         "class": css_class,
-        "width": 64,
-        "height": 64,
-    }
-
-
-@register.inclusion_tag("portfolio/media/display_detail.html")
-def display_detail(media: Media, css_class: str | None = None) -> dict[str, Any]:
-    width, height = media.dimensions
-    return {
-        "source_url": media.source.url,
-        "width": width,
-        "height": height,
+        "image": force_image if force_image else media.is_image,
+        "source_url": source_url,
         "detail_url": media.get_absolute_url(),
-        "alttext": media.title,
-        "image": media.is_image,
-        "class": css_class,
     }
