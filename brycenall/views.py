b95c1bc27c4c5b147d403b0436519cb78f685bc7
Blake Nall (darthnall) <dev@darthnall.com>
2024-11-28 12:10:26 -0600

    Added uv

--- brycenall/views.py
+++ brycenall/views.py
@@ -1,6 +1,4 @@
-from typing import Any
 from django.conf import settings
-from django.http import HttpRequest, HttpResponse
 from django.views.generic import TemplateView
 
 
@@ -12,25 +10,3 @@ class LandingView(TemplateView):
     }
     http_method_names = ["get"]
     template_name = "brycenall/landing.html"
-
-
-class BackgroundView(TemplateView):
-    content_type = "text/html"
-    http_method_names = ["get"]
-    template_name = "brycenall/background.html"
-
-    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        frame: int = request.session.get("frame", 0)
-        match frame:
-            case 23:
-                frame = 0
-            case _:
-                frame += 1
-        request.session["frame"] = frame
-
-        return super().get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
-        context: dict[str, Any] = super().get_context_data(**kwargs)
-        context["next_frame"] = self.request.session.get("frame", 0)
-        return context
