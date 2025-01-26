77478023ecbef3c676c1f61820114d7591ab1c05
darthnall <dev@darthnall.com>
2025-01-26 17:22:57 -0600

    Updated authentication form

--- brycenall/forms.py
+++ brycenall/forms.py
@@ -8,8 +8,8 @@ class AuthenticationForm(AuthForm):
     def __init__(self, request: HttpRequest | None = None, *args, **kwargs) -> None:
         super().__init__(request, *args, **kwargs)
         self.fields["username"].widget.attrs.update(
-            {"class": "m-4 p-4 grow rounded bg-white text-gray-800"}
+            {"class": "p-2 w-full block bg-gray-600 text-gray-50"}
         )
         self.fields["password"].widget.attrs.update(
-            {"class": "m-4 p-4 grow rounded bg-white text-gray-800"}
+            {"class": "p-2 w-full block bg-gray-600 text-gray-50"}
         )
